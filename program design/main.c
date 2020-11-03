58b8c684d29d386d4fb9b2b8f0856cef8245c163
guang-360 <duoguangxu@gmail.com>
2020-11-03 16:28:49 +0800

    Initial Commit

---
+++ program design/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  program design
+//
+//  Created by duoguang xu on 2020/11/3.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
